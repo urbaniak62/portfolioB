9685b8643062041069392400b283d74feaf221ac
urbaniak62 <urbaniaknicolas62@gmail.com>
2018-09-06 20:43:41 +0200

    merge

--- source/js/main.js
+++ source/js/main.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
                     pourcentage += + 2;
                 }
         $('.scrollBar').css('width', pourcentage + '%');
-        if (pourcentage > 21 && pourcentage < 30) {
+        if (pourcentage >= 18 && pourcentage < 31) {
             $('.header').addClass('header-black');
             $('.header-logo').addClass('header-display');
             $('.header-menu a ').addClass('display');
